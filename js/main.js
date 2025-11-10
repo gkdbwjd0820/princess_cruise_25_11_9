e613ebc0ae22d98476bcf56b8d6460a5db9f5409
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-11-10 10:27:16 +0900

    섹션2 해결

--- js/main.js
+++ js/main.js
@@ -1,40 +1,17 @@
 $(document).ready(function () {
-  var swiper = new Swiper(".sec_1_2", {
-    slidesPerView: 3.8,
-    spaceBetween: 8,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  var swiper = new Swiper(".travel_con", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 30,
+    loop: true,
+    coverflowEffect: {
+      rotate: 0,
+      // stretch: 0,
+      depth: 100,
+      // modifier: 1,
+      slideShadows: true,
     },
   });
-
-  var swiper = new Swiper(".sec_1_2_food", {
-    slidesPerView: 2.5,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  var swiper = new Swiper(".con_2_wrap", {
-    slidesPerView: 1.6,
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  var swiper = new Swiper(".sec_5", {
-    slidesPerView: 3.8,
-    spaceBetween: 0,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-
-
 });
